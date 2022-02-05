b8a9f25e16411bfe847fd23030ddbbc2c3946ee0
idrak sheikh <idraksheikh333@gmail.com>
2022-02-05 12:39:15 +0530

    updated

--- src/component/Switch.js
+++ src/component/Switch.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Switch() {
     
   return (<>
-        <div className='Switch bg-red-300 h-screen flex justify-center items-center'>
+        <div className='Switch bg-blue-300 h-screen flex justify-center items-center'>
              <label htmlFor='toggle'>
             <input type="checkbox" id="toggle" className='cursor-pointer h-32 w-64 rounded-full appearance-none bg-white border-2 border-black checked:bg-gray:600 transition duration-200' />
             </label> 
